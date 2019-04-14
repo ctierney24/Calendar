b55942ceca8e961a2f8b58d2985a70f6011c3917
Chris Tierney <chris.tierney24@gmail.com>
2019-04-14 17:19:46 -0400

    Cal populating with correct dates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,43 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'jsCalendar w/ Express' });
 });
+
+//Load the calendar based on the current day
+router.get('/loadCal', function(req, res){
+  let today = new Date();
+  let curMonth = today.getMonth();
+  let curYear = today.getYear();
+  let selMonth = req.param('monthList');
+  let selYear = req.param('yearList');
+
+  console.log('month='+selMonth+' year='+selYear);
+
+  //build array of calendar days for selected month
+  var day= new Date(selYear, selMonth, 1);
+  var days = {};
+  //initialize array to blank
+  for(let i=0; i<=5; i++){
+    days[i]=[];
+  };
+  var week=0;
+  while (day.getMonth() == selMonth){
+    days[week][day.getDay()] = new Date(day);
+
+    if (day.getDay() == 6){
+      week=week+1;
+    }
+    day.setDate(day.getDate() + 1);
+
+  }
+  //console.log(days);
+  //render index.js passing array of days
+  console.log('Render Response');
+  res.render('index', {title: 'jsCalendar w/ Express', days: days});
+
+});
+
+
 
 module.exports = router;
